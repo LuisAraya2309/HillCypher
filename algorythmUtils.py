4db4bc1dac007943f5089608381114178163c1d2
LuisAraya2309 <luistec2309@estudiantec.cr>
2022-10-21 09:51:31 -0600

    Decrypt algorythm ready, start testing

--- algorythmUtils.py
+++ algorythmUtils.py
@@ -36,3 +36,16 @@ def divideInParts(message : str, chunk : int) -> str:
         splittedMessage.extend([paddedMessage])
     
     return splittedMessage
+
+def translateMessage(DECRYPT_ALPHA : dict, matrixVectors : list)-> str:
+    translatedMessage = ""
+    for vector in matrixVectors:
+        for code in vector:
+            translatedMessage += DECRYPT_ALPHA[str(code)]
+            
+    return translatedMessage
+    
+def MCD(a , b):
+    if b == 0:
+        return a
+    return MCD(b, a % b)
